d6c31975f49183b779d9cfed282f2c76a2dae763
kayraegek18 <72798528+kayraegek18@users.noreply.github.com>
2022-05-24 21:54:38 +0300

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public SmoothBar bar;
+
+    private void Awake()
+    {
+        bar.OnBarValueChanged += OnBarChanged;
+    }
+
+    private void OnDisable()
+    {
+        bar.OnBarValueChanged -= OnBarChanged;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            bar.AddBarValue(10);
+        
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            bar.SubtractBarValue(10);
+    }
+
+    public void OnBarChanged(float value)
+    {
+        Debug.Log(value);
+    }
+}
